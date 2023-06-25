d9140e2847d5b79a97e2d86de05383a4280d24df
Oluwatoyosi <o.a.oyegoke@swansea.ac.uk>
2023-06-25 12:40:25 +0100

    implement invalid purchase exception

--- src/pairtest/lib/InvalidPurchaseException.js
+++ src/pairtest/lib/InvalidPurchaseException.js
@@ -1,3 +1,8 @@
 export default class InvalidPurchaseException extends Error {
-
+    constructor(message, data) {
+        super(message);
+        this.name = this.constructor.name;
+        this.message = message
+        this.data = data;
+    }
 }
